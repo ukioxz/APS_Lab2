106b19ece88b57504f5fcff345d18d88d7f2487f
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-06-20 14:30:09 +0200

    Add function for postfix calculation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,45 @@
 package lab2
 
-import "fmt"
+import (
+  "fmt"
+	"strconv"
+  "strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PostfixCalculation(input string) (string, error) {
+  stack := []int{}
+	result := 0
+  var elements = strings.Fields(input)
+
+  for _, el := range elements {
+    switch el {
+    case "+", "-", "*", "/":
+      topIdx := len(stack) -1
+      b := stack[topIdx]
+      a := stack[topIdx-1]
+      stack = stack[0: topIdx-1]
+      switch el {
+      case "+":
+        result = a + b
+      case "-":
+        result = a - b
+      case "*":
+        result = a * b
+      case "/":
+        result = a / b
+      }
+    default:
+      var err error
+      result, err = strconv.Atoi(el)
+      if err != nil {
+        return "0", fmt.Errorf("invalid input %s")
+      }
+    }
+    stack = append(stack, result)
+  }
+  if len(stack) != 1 {
+    return "0", fmt.Errorf("empty string %s")
+  }
+	res:= strconv.Itoa(result);
+  return res, nil
 }
