b1c98d7d9aaa64d9b050beb05bf776e1b088d387
ukioxz <ikatember@gmail.com>
2022-06-21 14:37:10 +0300

    Add tests for Postfix function

--- implementation_test.go
+++ implementation_test.go
@@ -12,10 +12,55 @@ type TestSuite struct{}
 
 var _ = Suite(&TestSuite{})
 
-func (s *TestSuite) TestPostfixCalculation(c *C) {
-  res, err := PostfixCalculation("7 2 +")
-	c.Assert(res, Equals, "9")
+func (s *TestSuite) TestPostfixCalculation1(c *C) {
+  res, err := PostfixCalculation("7 2 + 2 *")
+	c.Assert(res, Equals, "18")
 	c.Assert(err, IsNil)
+}
+
+func (s *TestSuite) TestPostfixCalculation2(c *C) {
+  res, err := PostfixCalculation("2 3 ^ 8 +")
+	c.Assert(res, Equals, "16")
+	c.Assert(err, IsNil)
+}
+
+func (s *TestSuite) TestPostfixCalculation3(c *C) {
+  res, err := PostfixCalculation("4 2 - 2 ^")
+	c.Assert(res, Equals, "4")
+	c.Assert(err, IsNil)
+}
+
+func (s *TestSuite) TestPostfixCalculation4(c *C) {
+  res, err := PostfixCalculation("2 3 * 1 + 7 3 + - 3 / 4 3 * + 8 -")
+	c.Assert(res, Equals, "3")
+	c.Assert(err, IsNil)
+}
+
+func (s *TestSuite) TestPostfixCalculation5(c *C) {
+  res, err := PostfixCalculation("12 3 * 6 / 18 3 * + 2 2 ^ /")
+	c.Assert(res, Equals, "15")
+	c.Assert(err, IsNil)
+}
+
+func (s *TestSuite) TestPostfixCalculation6(c *C) {
+  res, err := PostfixCalculation("20 3 * 6 / 12 3 * + 7 3 * 2 + /")
+	c.Assert(res, Equals, "2")
+	c.Assert(err, IsNil)
+}
+
+func (s *TestSuite) TestPostfixCalculation7(c *C) {
+	_, err := PostfixCalculation(" ")
+	c.Assert(err, ErrorMatches, "empty string")
+}
+
+func (s *TestSuite) TestPostfixCalculation8(c *C) {
+	_, err := PostfixCalculation("h + 2")
+	c.Assert(err, ErrorMatches, "invalid input")
+}
+
+func (s *TestSuite) TestPostfixCalculation9(c *C) {
+	_, err := PostfixCalculation("g o")
+	c.Assert(err, ErrorMatches, "invalid input")
 }
 
 func ExamplePostfixCalculation() {
