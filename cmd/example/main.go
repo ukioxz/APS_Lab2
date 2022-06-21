54a5aa137d943b85550b208ff4194a4caa332acb
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-06-21 11:30:27 +0200

    Add CI

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	lab2 "github.com/ukioxz/APS_Lab2"
 )
 
 var (
@@ -22,6 +22,6 @@ func main() {
 	//       }
 	//       err := handler.Compute()
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
+	res, _ := lab2.PostfixCalculation("2 2 +")
 	fmt.Println(res)
 }
